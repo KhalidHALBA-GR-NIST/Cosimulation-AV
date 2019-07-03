796724c57889e8bcf1e7aa983c1de3f2b8f7ef9d
KhalidHALBA-GR-NIST <khalid.halba@nist.gov>
2019-07-03 07:08:48 +0000

    Cosimulation AV COA

---
+++ AV_generated/AV-java-federates/AV-impl-java/Communication/src/main/java/AV/Communication.java
@@ -0,0 +1,144 @@
+package AV;
+
+import org.cpswt.config.FederateConfig;
+import org.cpswt.config.FederateConfigParser;
+import org.cpswt.hla.InteractionRoot;
+import org.cpswt.hla.base.AdvanceTimeRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The Communication type of federate for the federation designed in WebGME.
+ *
+ */
+public class Communication extends CommunicationBase {
+    private final static Logger log = LogManager.getLogger();
+
+    private double currentTime = 0;
+
+    public Communication(FederateConfig params) throws Exception {
+        super(params);
+    }
+
+    private void checkReceivedSubscriptions() {
+
+        InteractionRoot interaction = null;
+        while ((interaction = getNextInteractionNoWait()) != null) {
+            if (interaction instanceof CAN) {
+                handleInteractionClass((CAN) interaction);
+            }
+            else {
+                log.debug("unhandled interaction: {}", interaction.getClassName());
+            }
+        }
+     }
+
+    private void execute() throws Exception {
+        if(super.isLateJoiner()) {
+            log.info("turning off time regulation (late joiner)");
+            currentTime = super.getLBTS() - super.getLookAhead();
+            super.disableTimeRegulation();
+        }
+
+        /////////////////////////////////////////////
+        // TODO perform basic initialization below //
+        /////////////////////////////////////////////
+
+        AdvanceTimeRequest atr = new AdvanceTimeRequest(currentTime);
+        putAdvanceTimeRequest(atr);
+
+        if(!super.isLateJoiner()) {
+            log.info("waiting on readyToPopulate...");
+            readyToPopulate();
+            log.info("...synchronized on readyToPopulate");
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+        // TODO perform initialization that depends on other federates below //
+        ///////////////////////////////////////////////////////////////////////
+
+        if(!super.isLateJoiner()) {
+            log.info("waiting on readyToRun...");
+            readyToRun();
+            log.info("...synchronized on readyToRun");
+        }
+
+        startAdvanceTimeThread();
+        log.info("started logical time progression");
+
+        while (!exitCondition) {
+            atr.requestSyncStart();
+            enteredTimeGrantedState();
+
+            ////////////////////////////////////////////////////////////////////////////////////////
+            // TODO send interactions that must be sent every logical time step below.
+            // Set the interaction's parameters.
+            //
+            //    CAN vCAN = create_CAN();
+            //    vCAN.set_ACKslot( < YOUR VALUE HERE > );
+            //    vCAN.set_CRC( < YOUR VALUE HERE > );
+            //    vCAN.set_DLC( < YOUR VALUE HERE > );
+            //    vCAN.set_DataField( < YOUR VALUE HERE > );
+            //    vCAN.set_EndOfFrame( < YOUR VALUE HERE > );
+            //    vCAN.set_ID11B( < YOUR VALUE HERE > );
+            //    vCAN.set_ID18B( < YOUR VALUE HERE > );
+            //    vCAN.set_IDE( < YOUR VALUE HERE > );
+            //    vCAN.set_Parameter( < YOUR VALUE HERE > );
+            //    vCAN.set_RTR( < YOUR VALUE HERE > );
+            //    vCAN.set_ReservedBit1( < YOUR VALUE HERE > );
+            //    vCAN.set_ReservedBit2( < YOUR VALUE HERE > );
+            //    vCAN.set_SRR( < YOUR VALUE HERE > );
+            //    vCAN.set_StartOfFrame( < YOUR VALUE HERE > );
+            //    vCAN.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
+            //    vCAN.set_federateFilter( < YOUR VALUE HERE > );
+            //    vCAN.set_originFed( < YOUR VALUE HERE > );
+            //    vCAN.set_sourceFed( < YOUR VALUE HERE > );
+            //    vCAN.sendInteraction(getLRC(), currentTime + getLookAhead());
+            //
+            ////////////////////////////////////////////////////////////////////////////////////////
+
+            checkReceivedSubscriptions();
+
+            ////////////////////////////////////////////////////////////////////////////////////////
+            // TODO break here if ready to resign and break out of while loop
+            ////////////////////////////////////////////////////////////////////////////////////////
+
+
+            if (!exitCondition) {
+                currentTime += super.getStepSize();
+                AdvanceTimeRequest newATR = new AdvanceTimeRequest(currentTime);
+                putAdvanceTimeRequest(newATR);
+                atr.requestSyncEnd();
+                atr = newATR;
+            }
+        }
+
+        // call exitGracefully to shut down federate
+        exitGracefully();
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        // TODO Perform whatever cleanups needed before exiting the app
+        ////////////////////////////////////////////////////////////////////////////////////////
+    }
+
+    private void handleInteractionClass(CAN interaction) {
+        //////////////////////////////////////////////////////////////////////////
+        // TODO implement how to handle reception of the interaction            //
+        //////////////////////////////////////////////////////////////////////////
+    }
+
+    public static void main(String[] args) {
+        try {
+            FederateConfigParser federateConfigParser = new FederateConfigParser();
+            FederateConfig federateConfig = federateConfigParser.parseArgs(args, FederateConfig.class);
+            Communication federate = new Communication(federateConfig);
+            federate.execute();
+            log.info("Done.");
+            System.exit(0);
+        } catch (Exception e) {
+            log.error(e);
+            System.exit(1);
+        }
+    }
+}
